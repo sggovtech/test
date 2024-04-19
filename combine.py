dd2991a337a6e321851498fbd61b26d20b9c7738
shantanu561993@gmail.com <shantanu561993@gmail.com>
2024-04-20 01:42:54 +0800

    added combine and push to original repo

--- combine.py
+++ combine.py
@@ -1,1 +1,19 @@
 import pandas as pd
+import os
+
+for files in os.listdir():
+    if files.startswith('B_') and files.endswith('.csv'):
+        df = pd.read_csv(files)
+        if 'combined_df' not in locals():
+            combined_df = df
+        else:
+            combined_df = pd.concat([combined_df, df], ignore_index=True)
+combined_df.to_csv('combined_B_instruments.csv', index=False)
+for files in os.listdir():
+    if files.startswith('N_') and files.endswith('.csv'):
+        df = pd.read_csv(files)
+        if 'combined_df' not in locals():
+            combined_df = df
+        else:
+            combined_df = pd.concat([combined_df, df], ignore_index=True)
+combined_df.to_csv('combined_N_instruments.csv', index=False)
